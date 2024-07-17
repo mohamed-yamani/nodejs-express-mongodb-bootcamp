f39dae9524499e5621459f311cfc256e8b7e1813
Mohamed Yamani <myamani@Mohameds-MacBook-Pro-2.local>
2024-07-17 19:21:55 +0100

    writing data

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 
 const data = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(data);
+
+const textOut = `This is what we know about the avocado: ${data}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt', textOut);
